7e48a910472c38cfd3615169e5409bb66c71e9ee
Adam Young <13275708+Awy64@users.noreply.github.com>
2021-06-05 14:01:51 -0400

    Revert "Nav bar creation"

--- src/components/checklist.js
+++ src/components/checklist.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Checklist = () => {
+const checklist = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Checklist = () => {
   )
 }
 
-export default Checklist
+export default checklist
